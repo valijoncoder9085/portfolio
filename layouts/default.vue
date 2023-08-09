27e24fc1318f217b1d896caa1bdb5a3a4a27c2b6
Valijon Coder <valijoncoder9085@gmail.com>
2023-08-09 07:04:47 -0700

    change

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <SidebarComponents/>
+  </div>
+</template>
+
+<script>
+import SidebarComponents from "~/components/Sidebar.vue";
+
+export default {
+  name: 'DefaultLayout',
+  components: {SidebarComponents},
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang="scss">
+</style>
